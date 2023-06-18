9c4fb61986dbfedbbff323cd12a5fc5598925310
Szymon Zackiewicz <zacszym@gmail.com>
2023-06-18 22:36:38 +0200

    postgresql driver unlocked

--- src/main/resources/db/migration/migration/V5__Create_FRIDGE_table.sql
+++ src/main/resources/db/migration/migration/V5__Create_FRIDGE_table.sql
@@ -1,11 +1,14 @@
-/*--CREATE TABLE fridges (
---fridge_id SERIAL PRIMARY KEY,
---name VARCHAR(100) NOT NULL
---);
+CREATE TABLE fridges (
+fridge_id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL
+);
+
+/*
 
 --mysql
-*/
+
 create table fridges(
     fridge_id INT primary key AUTO_INCREMENT,
     name varchar(100) not null
 );
+*/
